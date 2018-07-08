893012d42429f3eec0176db2e1d00acd9d9a7567
Adam Ricahrdson <adam@example.com>
2018-07-07 21:47:38 -0400

    Finished quick version of rand-run

--- main.go
+++ main.go
@@ -2,25 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"math/rand"
 	"os"
 	"os/exec"
+	"strings"
+	"time"
 )
 
-func main() {
-	fmt.Println("Random Run!")
-
-	command := "c:/Users/richa/Documents/fba64_029743/fba64.exe"
-	flags := []string{"vsav", "-w"}
-
-	err := run(command, flags)
-	if err != nil {
-		log.Fatal(err)
-	}
+type runnable struct {
+	command string
+	flags   []string
 }
 
-func run(command string, flags []string) error {
-	cmd := exec.Command(command, flags...)
+func run(r runnable) error {
+	cmd := exec.Command(r.command, r.flags...)
 	cmd.Env = os.Environ()
 
 	if err := cmd.Start(); err != nil {
@@ -29,4 +26,74 @@ func run(command string, flags []string) error {
 
 	cmd.Wait()
 	return nil
+}
+
+func parseRunnableCSV(csvPath string) ([]runnable, error) {
+	content, err := ioutil.ReadFile(csvPath)
+	if err != nil {
+		return nil, err
+	}
+
+	lines := strings.Split(string(content), "\n")
+	runnables := []runnable{}
+	for _, l := range lines {
+		items := strings.Split(l, ",")
+		if len(items) < 2 {
+			continue
+		}
+		runnables = append(runnables, runnable{
+			command: items[0],
+			flags:   items[1:],
+		})
+	}
+	// TODO: return created runnable
+	/*	games := []string{"vsav", "invaders"}
+		runnables := []runnable{}
+		for _, g := range games {
+			r := runnable{
+				command: "c:/Users/richa/Documents/fba64_029743/fba64.exe",
+				flags:   []string{g, "-w"},
+			}
+			runnables = append(runnables, r)
+		}*/
+
+	return runnables, nil
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	fmt.Println("Random Run!")
+
+	runnables, err := parseRunnableCSV("test.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	randIndex := rand.Intn(len(runnables))
+	doneCh := make(chan struct{})
+	go func() {
+		err := run(runnables[randIndex])
+		close(doneCh)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	go func() {
+		totalSeconds := 0
+		for {
+			hours := totalSeconds / 3600
+			seconds := totalSeconds - (hours * 3600)
+			minutes := seconds / 60
+			seconds -= (minutes * 60)
+
+			fmt.Printf("\r%d hours, %d minutes, %d seconds", hours, minutes, seconds)
+			time.Sleep(time.Second)
+			totalSeconds += 1
+		}
+	}()
+
+	select {
+	case <-doneCh:
+		fmt.Println("\nGoodbye!")
+	}
 }
