b87544d959b8324cc17f4bb2149cbfd8a7d73dcd
Adam Richardson <adam@example.com>
2018-07-08 17:26:18 -0400

    Added boltdb store for run times

--- main.go
+++ main.go
@@ -8,8 +8,11 @@ import (
 	"math/rand"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 	"time"
+
+	"github.com/boltdb/bolt"
 )
 
 type Runnable struct {
@@ -51,6 +54,14 @@ func parseRunnableCSV(csvPath string) ([]Runnable, error) {
 	return runnables, nil
 }
 
+func getByteSliceFromInt(num int) []byte {
+	return []byte(strconv.Itoa(num))
+}
+
+func getIntFromByteSlice(bs []byte) (int, error) {
+	return strconv.Atoi(string(bs))
+}
+
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	csvPath := flag.String("csv", "test.csv", "Path runnable csv, first column is the command the rest are the flags")
@@ -61,28 +72,69 @@ func main() {
 		log.Fatal(err)
 	}
 	randIndex := rand.Intn(len(runnables))
-	doneCh := make(chan struct{})
+	runDoneCh := make(chan struct{})
 	go func() {
 		err := run(runnables[randIndex])
-		close(doneCh)
+		close(runDoneCh)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}()
-
+	r := runnables[randIndex]
+	runKey := fmt.Sprintf("%s %s", r.command, strings.Join(r.flags, " "))
+	doneCh := make(chan struct{})
 	go func() {
-		time.Sleep(time.Second * 1)
-		totalSeconds := 0
-		fmt.Println("\n***************************")
-		for {
-			hours := totalSeconds / 3600
-			seconds := totalSeconds - (hours * 3600)
-			minutes := seconds / 60
-			seconds -= (minutes * 60)
+		db, err := bolt.Open("rand-run.db", 0600, nil)
+		defer db.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+		timerDoneCh := make(chan struct{})
+		err = db.Update(func(tx *bolt.Tx) error {
+			b, err := tx.CreateBucketIfNotExists([]byte("rand-run-bucket"))
+			if err != nil {
+				return err
+			}
+			//			fmt.Println("\n*****\nBucket:", b)
+			time.Sleep(time.Second * 1)
+			//		fmt.Println("\n******\nRunKey:", runKey)
+			totalSeconds, err := getIntFromByteSlice(b.Get([]byte(runKey)))
+			if err != nil {
+				log.Println(err)
+				totalSeconds = 0
+			}
+			fmt.Println("\n***************************")
+			for {
+				select {
+				case <-runDoneCh:
+					close(timerDoneCh)
+					return nil
+				default:
+					hours := totalSeconds / 3600
+					seconds := totalSeconds - (hours * 3600)
+					minutes := seconds / 60
+					seconds -= (minutes * 60)
 
-			fmt.Printf("\r%d hours, %d minutes, %d seconds", hours, minutes, seconds)
-			time.Sleep(time.Second)
-			totalSeconds += 1
+					fmt.Printf("\r%d hours, %d minutes, %d seconds", hours, minutes, seconds)
+					time.Sleep(time.Second)
+					err := b.Put([]byte(runKey), getByteSliceFromInt(totalSeconds))
+					if err != nil {
+						close(timerDoneCh)
+						return err
+					}
+					totalSeconds += 1
+				}
+			}
+			return nil
+		})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		select {
+		case <-timerDoneCh:
+			close(doneCh)
 		}
 	}()
 
